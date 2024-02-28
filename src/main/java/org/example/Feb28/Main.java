959a91849fe5a0f5b518db8da1f6ea27d3e9ce93
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-02-28 08:34:09 -0500

    Add Googbye World print

--- src/main/java/org/example/Feb28/Main.java
+++ src/main/java/org/example/Feb28/Main.java
@@ -3,5 +3,6 @@ package org.example.Feb28;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Adiós mundo");
     }
 }
